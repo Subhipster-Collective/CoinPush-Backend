19a278c1aac64a87b1dfcb28235af44e7d2d54f7
Jeffrey Thomas Piercy <mqduck@mqduck.net>
2017-07-26 15:14:58 -0700

    Add user_info.js

---
+++ user_info.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+'use strict';
+
+const admin = require('firebase-admin');
+const serviceAccount = require('./coin-push-firebase-adminsdk-5s3qb-8b77683674.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://coin-push.firebaseio.com'
+});
+
+const db = admin.database();
+const ref = db.ref(admin.databaseURL).child('users');
+ 
+if(require.main === module)
+{
+    ref.on('value', users => users.forEach((user) => {
+        const childRef = ref.child(user.key);
+        if(user.child('added').val() === null)
+            childRef.child('added').set((new Date()).getTime());
+        if(user.child('askedToSupport').val() === null)
+            childRef.child('askedToSupport').set(false);
+    }));
+}
