b467e0c44baab92a75fb3abace73208d82af1798
Bijan Massoumi <bmassoumi@gmail.com>
2017-07-28 07:48:58 +0000

    add notification sending

--- push.js
+++ push.js
@@ -1,27 +1,84 @@
 const admin = require('firebase-admin');
-<<<<<<< Updated upstream
-const serviceAccount = require('./coin-push-firebase-adminsdk-5s3qb-8b77683674.json');
-=======
-
 const serviceAccount = require('./coin-push-firebase-adminsdk-5s3qb-8b77683674.json');
 
->>>>>>> Stashed changes
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: 'https://coin-push.firebaseio.com'
 });
+
 // Get a database reference to our posts
 const db = admin.database();
 const ref = db.ref(admin.databaseURL);
 
-if (require.main === module) {
-    // Attach an asynchronous callback to read the data at our posts reference
-    ref.child('users').once('value', (snapshot) => {
-        snapshot.forEach( (user) => {
-            console.log(user.val());
-        });
+const decreaseText = ['The current value of ',' has fallen ', ' percent.'];
+const increaseText = ['The current value of ',' has increased ', ' percent.'];
+let activeUsers = {};
 
-    }, (errorObject) => {
-        console.log('The read failed: ' + errorObject.code);
-    });
+
+ref.child('users').on('value', (snapshot) =>  {
+    activeUsers = snapshot.val();
+});
+
+ref.child('conversionData').on('value', (snapshot) => {
+    const currencyData = snapshot.val();
+    if (Object.keys(activeUsers).length !== 0) {
+        for (const id in activeUsers) {
+            const token = activeUsers[id].token;
+            for (const currencies in activeUsers[id].conversions) {
+                const bucket = activeUsers[id].conversions[currencies];
+                const fromCurr = currencies.split(':')[0];
+                const toCurr = currencies.split(':')[1];
+                const delta = (currencyData[fromCurr][toCurr].CHANGEPCT24HOUR);
+                handlePush(token,bucket,delta,fromCurr);
+            }
+        }
+    }
+
+}, (errorObject) => {
+    console.log('The read failed: ' + errorObject.code);
+
+});
+
+
+//helper functions
+
+function handlePush(token, userPref, delta, fromCurr) {
+    if (hasDecreased(delta) && userPref.pushDecreased
+    && userPref.thresholdDecreased < delta) {
+        delta = Number(delta.toString().substring(1));
+        sendNotification(token,decreaseText[0]
+                        + fromCurr + decreaseText[1]
+                        + userPref.thresholdDecreased + decreaseText[2]);
+
+    } else if (!hasDecreased(delta) && userPref.pushIncreased
+    && userPref.thresholdIncreased > delta) {
+        delta = Number(delta.toString().substring(1));
+        sendNotification(token,increaseText[0]
+                        + fromCurr + increaseText[1]
+                        + userPref.thresholdIncreased + increaseText[2]);
+    }
+}
+
+function sendNotification(token,messageText){
+
+    const payload = {
+        notification: {
+            title: 'alert',
+            body: messageText
+        }
+    };
+    console.log(token);
+    admin.messaging().sendToDevice(token, payload)
+  .then((response) => {
+      console.log('Successfully sent message:', response);
+  })
+  .catch((error) => {
+      console.log('Error sending message:', error);
+  });
+
+}
+
+function hasDecreased(delta) {
+    const temp = delta.toString();
+    return (temp[0] === '-');
 }
