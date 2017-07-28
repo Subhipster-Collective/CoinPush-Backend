9716f71bb5671cc287bc8b2421d2d1094f6c6ecc
Jeffrey Thomas Piercy <mqduck@mqduck.net>
2017-07-28 02:38:44 +0000

    Only put conversion data we need into the database.

--- conversion_data.js
+++ conversion_data.js
@@ -2,10 +2,9 @@
 'use strict';
 
 const UPDATE_DELAY = 10000;
-const CRYPTOCURRENCIES = 'ETH,BTC,LTC,DASH,XMR,NXT,ZEC,DGB,XRP,ETC';
-const NONCRYPTOCURRENCIES = 'USD,EUR,GBP,JPY,CNY,AUD,CAD,CHF';
-const URL = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=' + CRYPTOCURRENCIES + '&tsyms=' + 
-    CRYPTOCURRENCIES + ',' + NONCRYPTOCURRENCIES;
+const CRYPTOCURRENCIES = ['ETH', 'BTC', 'LTC', 'DASH', 'XMR', 'NXT', 'ZEC', 'DGB', 'XRP', 'ETC'];
+const ALL_CURRENCIES = CRYPTOCURRENCIES.concat(['USD', 'EUR', 'GBP', 'JPY', 'CNY', 'AUD', 'CAD', 'CHF']);
+const URL = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=' + CRYPTOCURRENCIES + '&tsyms=' + ALL_CURRENCIES;
 
 const admin = require('firebase-admin');
 const serviceAccount = require('./coin-push-firebase-adminsdk-5s3qb-8b77683674.json');
@@ -27,7 +26,22 @@ const updater = AsyncPolling((end) => {
     }, (error, response, body) => {
         if (!error && response.statusCode === 200)
         {
-            ref.update(body.RAW);
+            const raw = body.RAW;
+            const data = {};
+            for(const currencyFrom of CRYPTOCURRENCIES)
+            {
+                data[currencyFrom] = {};
+                const dataFrom = data[currencyFrom];
+                const cryptocompareFrom = raw[currencyFrom];
+                for(const currencyTo of ALL_CURRENCIES)
+                {
+                    dataFrom[currencyTo] = {
+                        PRICE: cryptocompareFrom[currencyTo].PRICE,
+                        CHANGEPCT24HOUR: cryptocompareFrom[currencyTo].CHANGEPCT24HOUR
+                    };
+                }
+            }
+            ref.update(data);
             ref.update({timestamp: (new Date()).getTime()});
         }
     });
