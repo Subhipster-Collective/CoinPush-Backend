cdc078c8cbac3f080b50241289dbcbedef25e4ba
Jeffrey Thomas Piercy <mqduck@mqduck.net>
2017-11-29 03:17:36 +0000

    Change where historic data is put in Firebase.

--- conversion_data_historic.js
+++ conversion_data_historic.js
@@ -26,8 +26,8 @@ const request = require('request');
 const currencies = require('./currencies.js');
 
 const SECS_PER_DAY = 86400;
-const MAX_CURRENCIES_TO = 7;
-const PAUSE_TIME = 120;
+const MAX_CURRENCY_TOS = 7;
+const PAUSE_TIME = 150;
 
 function copyChild(refOld, refNew)
 {
@@ -45,32 +45,30 @@ admin.initializeApp({
 });
 
 const db = admin.database();
-const ref = db.ref(admin.databaseURL).child('conversionData');
+const ref = db.ref(admin.databaseURL).child('conversionData').child('historical');
 const now = (new Date()).getTime() / 1000 | 0;
 
-async function pushData(time, dayRef)
+async function pushData(time, day, data)
 {
-    const data = {};
     for(const currencyFrom of currencies.CRYPTOCURRENCIES)
     {
-        data[currencyFrom] = {};
+        if(typeof data[currencyFrom] === 'undefined')
+            data[currencyFrom] = {};
         let currencyTos = [];
         for(let i = 0; i < currencies.ALL_CURRENCIES.length; ++i)
         {
             currencyTos.push(currencies.ALL_CURRENCIES[i]);
-            if(i % MAX_CURRENCIES_TO === MAX_CURRENCIES_TO - 1 || i === currencies.ALL_CURRENCIES.length - 1)
+            if(i % MAX_CURRENCY_TOS === MAX_CURRENCY_TOS - 1 || i === currencies.ALL_CURRENCIES.length - 1)
             {
-                getConversion(currencyFrom, currencyTos, time, data);
+                getConversion(currencyFrom, currencyTos, day, time, data);
                 currencyTos = [];
                 await new Promise(resolve => setTimeout(resolve, PAUSE_TIME));
             }
         }
     }
-    dayRef.update(data);
-    dayRef.update({timestamp: time * 1000});
 }
 
-function getConversion(currencyFrom, currencyTos, time, data)
+function getConversion(currencyFrom, currencyTos, day, time, data)
 {
     const URL = `https://min-api.cryptocompare.com/data/pricehistorical?fsym=${currencyFrom}&tsyms=${currencyTos}&ts=${time}`;
     request({
@@ -79,33 +77,43 @@ function getConversion(currencyFrom, currencyTos, time, data)
     }, (error, response, body) => {
         if (!error && response.statusCode === 200)
             for(const currencyTo of currencyTos)
-                data[currencyFrom][currencyTo] = { PRICE: (currencyFrom === currencyTo) ? 1 : body[currencyFrom][currencyTo] };
+            {
+                if(typeof data[currencyFrom][currencyTo] === 'undefined')
+                    data[currencyFrom][currencyTo] = {};
+                data[currencyFrom][currencyTo][day] = {
+                    PRICE: (currencyFrom === currencyTo) ? 1 : body[currencyFrom][currencyTo],
+                    timestamp: time * 1000 
+                };
+            }
     });
 }
 
 async function main()
 {
-    await pushData(now -    SECS_PER_DAY, ref.child('day1'));
-    await pushData(now -  2*SECS_PER_DAY, ref.child('day2'));
-    await pushData(now -  3*SECS_PER_DAY, ref.child('day3'));
-    await pushData(now -  4*SECS_PER_DAY, ref.child('day4'));
-    await pushData(now -  5*SECS_PER_DAY, ref.child('day5'));
-    await pushData(now -  6*SECS_PER_DAY, ref.child('day6'));
-    await pushData(now -  7*SECS_PER_DAY, ref.child('day7'));
+    const data = {};
     
-    copyChild(ref.child('day7'), ref.child('week1'));
-    await pushData(now - 14*SECS_PER_DAY, ref.child('week2'));
-    await pushData(now - 21*SECS_PER_DAY, ref.child('week3'));
-    await pushData(now - 28*SECS_PER_DAY, ref.child('week4'));
-    await pushData(now - 35*SECS_PER_DAY, ref.child('week5'));
-    await pushData(now - 42*SECS_PER_DAY, ref.child('week6'));
-    await pushData(now - 49*SECS_PER_DAY, ref.child('week7'));
-    await pushData(now - 56*SECS_PER_DAY, ref.child('week8'));
-    await pushData(now - 63*SECS_PER_DAY, ref.child('week9'));
-    await pushData(now - 70*SECS_PER_DAY, ref.child('week10'));
-    await pushData(now - 77*SECS_PER_DAY, ref.child('week11'));
-    await pushData(now - 84*SECS_PER_DAY, ref.child('week12'));
+    await pushData(now -    SECS_PER_DAY, 'day1', data);
+    await pushData(now -  2*SECS_PER_DAY, 'day2', data);
+    await pushData(now -  3*SECS_PER_DAY, 'day3', data);
+    await pushData(now -  4*SECS_PER_DAY, 'day4', data);
+    await pushData(now -  5*SECS_PER_DAY, 'day5', data);
+    await pushData(now -  6*SECS_PER_DAY, 'day6', data);
+    await pushData(now -  7*SECS_PER_DAY, 'day7', data);
     
+    //copyChild(ref.child('day7'), ref.child('week1'));
+    await pushData(now - 14*SECS_PER_DAY, 'week2', data);
+    await pushData(now - 21*SECS_PER_DAY, 'week3', data);
+    await pushData(now - 28*SECS_PER_DAY, 'week4', data);
+    await pushData(now - 35*SECS_PER_DAY, 'week5', data);
+    await pushData(now - 42*SECS_PER_DAY, 'week6', data);
+    await pushData(now - 49*SECS_PER_DAY, 'week7', data);
+    await pushData(now - 56*SECS_PER_DAY, 'week8', data);
+    await pushData(now - 63*SECS_PER_DAY, 'week9', data);
+    await pushData(now - 70*SECS_PER_DAY, 'week10', data);
+    await pushData(now - 77*SECS_PER_DAY, 'week11', data);
+    await pushData(now - 84*SECS_PER_DAY, 'week12', data);
+    
+    ref.update(data);
     admin.app().delete();
 }
 
